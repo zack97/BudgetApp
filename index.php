25e1f4e7744235e15350bc4c60ff0aeb7c530409
zack10 <tshibzack@gmail.com>
2024-07-25 04:27:10 +0200

    updating the pdf displays 4

--- index.php
+++ index.php
@@ -61,95 +61,26 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 // Determine which expense (if any) to edit
 $editingExpense = isset($editIndex) ? $_SESSION['expenses'][$editIndex] : null;
+
+// Output variables
+$totalAmount = htmlspecialchars($_SESSION['total_amount']);
+$expenditureValue = htmlspecialchars($_SESSION['expenditure_value']);
+$balanceAmount = htmlspecialchars($_SESSION['balance_amount']);
+$expenses = $_SESSION['expenses'];
+$editIndex = isset($editIndex) ? htmlspecialchars($editIndex) : '';
+$editingExpenseTitle = isset($editingExpense['title']) ? htmlspecialchars($editingExpense['title']) : '';
+$editingExpenseAmount = isset($editingExpense['amount']) ? htmlspecialchars($editingExpense['amount']) : '';
 ?>
 
-
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
     <title>Budget App | PHP Version</title>
-</head>
-<body>
-<div class="wrapper">
-    <div class="container">
-        <div class="sub-container">
-            <div class="total-amount-container">
-                <h3>Budget</h3>
-                <?php if (isset($budgetError)): ?>
-                    <p class="error"><?php echo htmlspecialchars($budgetError); ?></p>
-                <?php endif; ?>
-                <form method="post" action="">
-                    <input type="number" name="total_amount" placeholder="Enter Total Amount" required>
-                    <button class="submit" type="submit" name="set_budget">Set Budget</button>
-                </form>
-            </div>
-
-            <div class="user-amount-container">
-                <h3>Expenses</h3>
-                <?php if (isset($expenseError)): ?>
-                    <p class="error"><?php echo htmlspecialchars($expenseError); ?></p>
-                <?php endif; ?>
-                <form method="post" action="">
-                    <input type="text" name="expense_title" placeholder="Enter Title Of Product" required>
-                    <input type="number" name="expense_amount" placeholder="Enter Cost Of Product" required>
-                    <button class="submit" type="submit" name="add_expense">Check Amount</button>
-                </form>
-            </div>
-        </div>
-
-        <div class="output-container flex-space">
-            <div>
-                <p>Total Budget</p>
-                <span id="amount"><?php echo htmlspecialchars($_SESSION['total_amount']); ?></span>
-            </div>
-            <div>
-                <p>Expenses</p>
-                <span id="expenditure-value"><?php echo htmlspecialchars($_SESSION['expenditure_value']); ?></span>
-            </div>
-            <div>
-                <p>Balance</p>
-                <span id="balance-amount"><?php echo htmlspecialchars($_SESSION['balance_amount']); ?></span>
-            </div>
-        </div>
-    </div>
-
-    <div class="list">
-        <h3>Expenses List</h3>
-        <div class="list-container" id="list">
-            <?php foreach ($_SESSION['expenses'] as $index => $expense): ?>
-                <div class="sublist-content flex-space">
-                    <p class="product"><?php echo htmlspecialchars($expense['title']); ?></p>
-                    <p class="amount"><?php echo htmlspecialchars($expense['amount']); ?></p>
-                    <form method="post" action="" style="display:inline;">
-                        <input type="hidden" name="index" value="<?php echo $index; ?>">
-                        <button class="delete" type="submit" name="delete_expense">Delete</button>
-                    </form>
-                    <form method="post" action="" style="display:inline;">
-                        <input type="hidden" name="edit_index" value="<?php echo $index; ?>">
-                        <button class="edit" type="submit">Edit</button>
-                    </form>
-                </div>
-            <?php endforeach; ?>
-        </div>
-    </div>
-</div>
-
-<?php if ($editingExpense): ?>
-    <div class="edit-form">
-        <h3>Edit Expense</h3>
-        <form method="post" action="">
-            <input type="hidden" name="index" value="<?php echo htmlspecialchars($editIndex); ?>">
-            <input type="text" name="expense_title" value="<?php echo htmlspecialchars($editingExpense['title']); ?>" placeholder="Enter Title Of Product" required>
-            <input type="number" name="expense_amount" value="<?php echo htmlspecialchars($editingExpense['amount']); ?>" placeholder="Enter Cost Of Product" required>
-            <button class="submit" type="submit" name="edit_expense">Save Changes</button>
-        </form>
-    </div>
-<?php endif; ?>
-
-<div class="download-pdf">
-    <a href="generate_pdf.php" target="_blank" class="download-button">Download PDF</a>
-</div>
-
-<style>
+    <style>
     .edit-form {
         display: block;
         position: fixed;
@@ -173,5 +104,87 @@ $editingExpense = isset($editIndex) ? $_SESSION['expenses'][$editIndex] : null;
         border-radius: 5px;
     }
 </style>
+</head>
+<body>
+<div class="wrapper">
+    <div class="container">
+        <div class="sub-container">
+            <div class="total-amount-container">
+                <h3>Budget</h3>
+                <?php if (isset($budgetError)): ?>
+                    <p class="error"><?php echo $budgetError; ?></p>
+                <?php endif; ?>
+                <form method="post" action="">
+                    <input type="number" name="total_amount" placeholder="Enter Total Amount" required>
+                    <button class="submit" type="submit" name="set_budget">Set Budget</button>
+                </form>
+            </div>
+
+            <div class="user-amount-container">
+                <h3>Expenses</h3>
+                <?php if (isset($expenseError)): ?>
+                    <p class="error"><?php echo $expenseError; ?></p>
+                <?php endif; ?>
+                <form method="post" action="">
+                    <input type="text" name="expense_title" placeholder="Enter Title Of Product" required>
+                    <input type="number" name="expense_amount" placeholder="Enter Cost Of Product" required>
+                    <button class="submit" type="submit" name="add_expense">Check Amount</button>
+                </form>
+            </div>
+        </div>
+
+        <div class="output-container">
+            <div>
+                <p>Total Budget</p>
+                <span><?php echo $totalAmount; ?></span>
+            </div>
+            <div>
+                <p>Expenses</p>
+                <span><?php echo $expenditureValue; ?></span>
+            </div>
+            <div>
+                <p>Balance</p>
+                <span><?php echo $balanceAmount; ?></span>
+            </div>
+        </div>
+    </div>
+
+    <div class="list">
+        <h3>Expenses List</h3>
+        <div class="list-container">
+            <?php foreach ($expenses as $index => $expense): ?>
+                <div class="sublist-content">
+                    <p class="product"><?php echo $expense['title']; ?></p>
+                    <p class="amount"><?php echo $expense['amount']; ?></p>
+                    <form method="post" action="" style="display:inline;">
+                        <input type="hidden" name="index" value="<?php echo $index; ?>">
+                        <button class="submit" type="submit" name="delete_expense">Delete</button>
+                    </form>
+                    <form method="post" action="" style="display:inline;">
+                        <input type="hidden" name="edit_index" value="<?php echo $index; ?>">
+                        <button class="submit" type="submit">Edit</button>
+                    </form>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
+</div>
+
+<?php if ($editingExpense): ?>
+    <div class="edit-form">
+        <h3>Edit Expense</h3>
+        <form method="post" action="">
+            <input type="hidden" name="index" value="<?php echo $editIndex; ?>">
+            <input type="text" name="expense_title" value="<?php echo $editingExpenseTitle; ?>" placeholder="Enter Title Of Product" required>
+            <input type="number" name="expense_amount" value="<?php echo $editingExpenseAmount; ?>" placeholder="Enter Cost Of Product" required>
+            <button class="submit" type="submit" name="edit_expense">Save Changes</button>
+        </form>
+    </div>
+<?php endif; ?>
+
+<div class="download-pdf">
+    <a href="generate_pdf.php" target="_blank" class="download-button">Download PDF</a>
+</div>
 
 </body>
+</html>
